9ad6943d76824f031d641c82ec7c000d15097d52
Rohit Rawat <rohitrawat26@gmail.com>
2025-04-04 11:20:03 +0530

    feat: dummy agent

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # doc-analysis-agentic
+
 A agentic document analyzer using LangGraph
+
+## Introduction
+
+LangGraph is a framework developed by LangChain to manage the control flow of applications that integrate an LLM. It provides more control over the execution flow of the agent.
+
+- Complex reasoning requiring controlled execution flow  
+- Stateful, multi-step processes  
+- Integration of deterministic logic with AI  
+- Workflows involving human oversight
---
+++ src/dummy.py
@@ -0,0 +1,69 @@
+## Components of LangGraph
+
+# 1. State
+# State is the central concept in LangGraph. It represents all the information that flows through your application.
+
+from typing_extensions import TypedDict
+
+class State(TypedDict):
+    graph_state: str
+
+# 2. Nodes
+# Nodes are python functions. 
+
+# Takes the state as input
+# Performs some operation
+# Returns updates to the state
+def node_1(state):
+    print("---Node 1---")
+    return {"graph_state": state['graph_state'] +" I am"}
+
+def node_2(state):
+    print("---Node 2---")
+    return {"graph_state": state['graph_state'] +" happy!"}
+
+def node_3(state):
+    print("---Node 3---")
+    return {"graph_state": state['graph_state'] +" sad!"}
+
+
+# 3. Edges
+# Edges connect nodes and define the possible paths through your graph.
+
+import random
+from typing import Literal
+
+def decide_mood(state) -> Literal["node_2", "node_3"]:
+    
+    # Often, we will use state to decide on the next node to visit
+    user_input = state['graph_state'] 
+    
+    # Here, let's just do a 50 / 50 split between nodes 2, 3
+    if random.random() < 0.5:
+
+        # 50% of the time, we return Node 2
+        return "node_2"
+    
+    # 50% of the time, we return Node 3
+    return "node_3"
+
+
+# 4. StateGraph
+# The StateGraph is the container that holds your entire agent workflow.
+
+from langgraph.graph import StateGraph, START, END
+
+# Build graph
+builder = StateGraph(State)
+builder.add_node("node_1", node_1)
+builder.add_node("node_2", node_2)
+builder.add_node("node_3", node_3)
+
+# Logic
+builder.add_edge(START, "node_1")
+builder.add_conditional_edges("node_1", decide_mood)
+builder.add_edge("node_2", END)
+builder.add_edge("node_3", END)
+
+# Add
+graph = builder.compile()
